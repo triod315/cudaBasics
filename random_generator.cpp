19533821382d2bc63cd49678d199112d7f904afc
triod315 <triod315@gmail.com>
2019-07-12 19:47:13 +0300

    Generation of numbers for testing

---
+++ random_generator.cpp
@@ -0,0 +1,39 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> genRandomArray(int n){
+    vector<int> resultv;
+
+    for (int i=0;i<n;i++)
+    {
+        resultv.push_back(rand()%1000);
+    }
+
+    return resultv;
+}
+
+void wiriteVector(vector<int> vec){
+    ofstream out("samples.txt",ios_base::app);
+    out<<vec.size()<<endl;
+    for (int i=0;i<vec.size();i++){
+        out<<vec[i]<<" ";
+    }
+    out<<endl;
+    
+}
+
+int main(int argc, char ** argv){
+    int sample_count=stoi(argv[1]);
+    int step=stoi(argv[2]);
+
+    for (int i=0;i<sample_count;i++){
+        wiriteVector(genRandomArray(step));
+        step+=step;
+    }
+
+    srand(time(NULL));
+    
+}
